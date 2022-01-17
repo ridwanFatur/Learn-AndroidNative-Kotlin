c6e1578d65358ba2b0e83e39171cb66a4f8733d9
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-17 17:00:15 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.ridwanproduction.learnandroidkotlin
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btnRequest).setOnClickListener {
+            simpleRequest().execute();
+        };
+    }
+
+    inner class simpleRequest() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<TextView>(R.id.errorText).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).text = ""
+            findViewById<TextView>(R.id.dataText).visibility = View.GONE
+            findViewById<TextView>(R.id.dataText).text = ""
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response: String?
+            try {
+                response =
+                    URL("https://jsonplaceholder.typicode.com/todos/1").readText(
+                        Charsets.UTF_8
+                    )
+            } catch (e: Exception) {
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (result != null){
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<TextView>(R.id.dataText).visibility = View.VISIBLE
+                findViewById<TextView>(R.id.dataText).text = result
+            }else{
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+                findViewById<TextView>(R.id.errorText).text = "Error"
+            }
+        }
     }
 }
