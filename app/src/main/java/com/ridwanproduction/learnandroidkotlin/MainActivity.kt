fa7213cd37f1104414df64148cff0651536387a2
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-17 17:51:17 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.ridwanproduction.learnandroidkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnNextDrawing: Button = findViewById(R.id.btnNextDrawing);
+        btnNextDrawing.setOnClickListener {
+            var intent: Intent = Intent(this, DrawingActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btnCustomDrawing).setOnClickListener {
+
+        }
     }
 }
