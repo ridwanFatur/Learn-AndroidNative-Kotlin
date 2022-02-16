ecfbc0027966846d9ed5adf47c807ce13c8cf366
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-16 14:22:04 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ridwanproduction.learnandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonNotif).setOnClickListener {
+            startService()
+        }
+    }
+
+    fun startService(){
+        val intent = Intent(this, MyService::class.java)
+        startService(intent)
     }
 }
