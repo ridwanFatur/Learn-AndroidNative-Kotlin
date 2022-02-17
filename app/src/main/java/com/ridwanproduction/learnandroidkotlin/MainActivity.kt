09b24cc80e894c82d8b8d72a6c476990e9004a97
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-17 13:59:53 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ridwanproduction.learnandroidkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnNextJavaActivity: Button = findViewById(R.id.btnNextJava);
+        btnNextJavaActivity.setOnClickListener {
+            var intent: Intent = Intent(this, NextJavaActivity::class.java)
+            startActivity(intent)
+        }
+
+        var btnNextKotlinActivity: Button = findViewById(R.id.btnNextKotlin);
+        btnNextKotlinActivity.setOnClickListener {
+            var intent: Intent = Intent(this, NextKotlinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
