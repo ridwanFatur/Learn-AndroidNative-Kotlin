7f2883a97db959810c3495c2021d2fd161123378
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-17 14:10:20 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/NextJavaActivity.java
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/NextJavaActivity.java
@@ -2,13 +2,44 @@ package com.ridwanproduction.learnandroidkotlin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
 public class NextJavaActivity extends AppCompatActivity {
+    private ArFragment arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_java);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        // adding listener for detecting plane
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+
+            // adding model to the scene
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("TocoToucan.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable));
+        });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode node = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem()); //  for moving, resizing object
+        transformableNode.setParent(node); // need to attach to parent
+        transformableNode.setRenderable(modelRenderable);
+
+        arFragment.getArSceneView().getScene().addChild(node); // adding only parent node, so the child nodes will be added automatically
+        transformableNode.select();
     }
 }
