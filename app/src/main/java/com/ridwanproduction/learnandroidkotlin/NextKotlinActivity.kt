90b434bf2358df7d78b721ec63655f47cc41eecc
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-17 15:23:15 +0700

    update

---
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/NextKotlinActivity.kt
@@ -0,0 +1,11 @@
+package com.ridwanproduction.learnandroidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_kotlin)
+    }
+}
