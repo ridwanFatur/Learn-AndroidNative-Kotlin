4b02734ed52aa948a7a090dc380ea68e8dbec09f
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-18 14:46:59 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/RecipeActivity.kt
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/RecipeActivity.kt
@@ -3,9 +3,9 @@ package com.ridwanproduction.learnandroidkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class NextKotlinActivity : AppCompatActivity() {
+class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_next_kotlin)
+        setContentView(R.layout.activity_recipe)
     }
 }
--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/RegisterActivity.kt
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/RegisterActivity.kt
@@ -1,15 +1,11 @@
 package com.ridwanproduction.learnandroidkotlin
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var postViewModel : RecipeViewModel
-
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
