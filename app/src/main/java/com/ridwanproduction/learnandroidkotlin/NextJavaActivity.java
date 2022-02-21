2e36f434cfd427fc8266034513b158a57b192258
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-21 12:11:07 +0700

    update

--- app/src/main/java/com/ridwanproduction/learnandroidkotlin/NextJavaActivity.java
+++ app/src/main/java/com/ridwanproduction/learnandroidkotlin/NextJavaActivity.java
@@ -1,14 +1,46 @@
 package com.ridwanproduction.learnandroidkotlin;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class NextJavaActivity extends AppCompatActivity {
-
+    static final int CAMERA_REQUEST = 99;
+    ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_java);
+
+        Button btnCamera = findViewById(R.id.btnCamera);
+        imgView = findViewById(R.id.imgView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imgView.setImageBitmap(bitmap);
+        }
     }
 }
